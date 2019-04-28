0c2c0302e3ad5fc11eee9062e21876c230322c9e
Chuyun Huang <chuyunhuang@pursuit.org>
2019-04-27 20:09:20 -0400

    implemented claimAction pickup and shipIt pages

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Home from './Component/home';
 import FoundItemForm from './Container/FoundItemForm';
 import LostItemForm from './Container/LostItemForm';
 import ItemMatch from './Container/itemMatch';
+import ClaimAction from './Component/claimAction';
+import PickUp from './Container/pickUp';
 
 
 function App() {
@@ -13,9 +15,12 @@ function App() {
     <HashRouter>
       <Switch>
       <Route path= "/" exact strict component={Home} />
-      <Route path = "/found" exact strict component={FoundItemForm} />
+      <Route path= "/found" exact strict component={FoundItemForm} />
       <Route path= "/lost" exact strict component ={LostItemForm} />
       <Route path= "/itemmatch" exact strict component ={ItemMatch} />
+      <Route path= "/claimPage" exact strict component ={ClaimAction} />
+      <Route path= "/pickup" exact strict component={PickUp} />
+  
       </Switch>
     </HashRouter>
  
--- src/Container/FoundItemForm.js
+++ src/Container/FoundItemForm.js
@@ -3,16 +3,84 @@ import {Link} from 'react-router-dom';
 import './Style/mainForm.css';
 import Img from '../Component/Image/boy.jpg';
 import Logo from '../Component/Image/findIt.jpg';
+import axios from 'axios';
+
 
 class FoundItemForm extends React.Component{
   constructor(props){
     super(props)
     this.state={
-
+      title: '',
+      type: [],
+      color: [],
+      location: '',
+      date: '', 
     }
   }
 
+  componentDidMount(){
+    axios({
+      url: 'https://findit1.herokuapp.com/type/read',
+      method: 'get'
+    })
+    .then((data)=>{
+      this.setState({
+        type: data.data.msg
+      })
+    axios({
+      url: 'https://findit1.herokuapp.com/colour/read',
+      method: 'get'
+    })
+    .then((data)=>{
+      this.setState({
+        color: data.data.msg
+      })
+    })
+    })
+  }
+
+  handleInput = (e) =>{
+    this.setState({
+      title: e.target.value
+    })
+  }
+
+  handleLocation = (e) =>{
+    this.setState({
+      location: e.target.value
+    })
+  }
+
+  handleDate = (e) =>{
+    this.setState({
+      date: e.target.value
+    })
+  }
+
+  handleClick = () =>{
+    axios({
+      url: 'https://findit1.herokuapp.com/items/create',
+      method: 'post',
+      data: {
+          "user_id": '2',
+          "type_id": this.state.type.value ,
+          "colour_id": this.state.color.value,
+          "lost_location": this.state.location,
+          "date": this.state.date,
+          "status": 'unclaimed', 
+          "fedex_location": 'pending',
+          "title": this.state.title, 
+          "img_url": 'https://tcprd-tdcdn.netdna-ssl.com/green-acrylic-green-london-tartan-scarf-240375-105-1600-0.jpg?id=SC240375' 
+      }
+    })
+    .then((res)=>{
+      console.log('after post', res)
+    })
+  }
+
   render(){
+    
+
     return (
       <>
       <div className="container">
@@ -21,56 +89,64 @@ class FoundItemForm extends React.Component{
         <div style={{paddingBottom:"30px"}}>
           <h1>Found Item Info
           <Link to="/">
-          <img src={Logo} className="img-thumbnail float-right" style={{width:'100px'}}/>
+          <img src={Logo} className="img-thumbnail float-right" style={{width:'100px'}} alt=""/>
           </Link>
           </h1>
         </div>
         <div className="form-group ">
-          <label for="inputEmail3">Title</label>
-            <input type="text" className="form-control" placeholder="Description" ></input>
+          <label>Title</label>
+            <input type="text" className="form-control" placeholder="Description" onChange={this.handleInput}></input>
         </div>
         <div className="form-group">
-          <label for="inputState">TYPE</label>
+          <label>TYPE</label>
           <select id="inputState" className="form-control">
-            <option selected>Choose...</option>
-            <option>Black</option>
-            <option>Brown</option>
-            <option>Grey</option>
-            <option>White</option>
-            <option>None of the above</option>
+            <option>Choose...</option>
+            {
+              this.state.type.map((e, i)=>{
+                return (
+                  <option key={i} value={e.id}>
+                      {e.type}
+                  </option>
+                ) 
+              })
+            }
           </select>
         </div>
 
         <div className="form-group">
-          <label for="inputState">COLOR</label>
+          <label>COLOR</label>
           <select id="inputState" className="form-control">
-            <option selected>Choose...</option>
-            <option>Keys</option>
-            <option>Wallet</option>
-            <option>Clothing Accessories</option>
-            <option>Toys</option>
-            <option>Electronics</option>
+            <option>Choose...</option>
+             {
+               this.state.color.map((e, i)=>{
+                return ( 
+                  <option key={i} value={e.id}>
+                      {e.colour}
+                  </option>
+               )
+             })
+            }
           </select>
         </div>
 
         <div className="form-group ">
-          <label for="inputEmail3">LOCATION</label>
-            <input type="email" className="form-control" placeholder="Zip Code" ></input>
+          <label>LOCATION</label>
+            <input type="email" className="form-control" placeholder="Zip Code" onChange={this.handleLocation} ></input>
         </div>
 
         <div className="form-group">
-          <label for="inputEmail3">DATE FOUND</label>
-            <input type="email" className="form-control" placeholder="DD/MM/YY" ></input>
+          <label>DATE FOUND</label>
+            <input type="email" className="form-control" placeholder="DD/MM/YY" onChange={this.handleDate} ></input>
         </div>
 
         {/* NTH using Google's Places API */}
         <div className="form-group">
-          <label for="inputEmail3">Search nearby Fedex Drop Off Location</label>
+          <label>Search nearby Fedex Drop Off Location</label>
             <input type="email" className="form-control" placeholder="current location/zip code" ></input>
         </div>
 
         <div className="form-group">
-          <button type="button" class="btn btn-danger">Submit</button>
+          <button type="button" className="btn btn-danger" onClick={this.handleClick}>Submit</button>
         </div>
       
         </form>
