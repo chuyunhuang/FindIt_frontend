2d03eecdb9f8f57cf0b431043548b3fcfebfb01b
Van Tran <vantran@pursuit.org>
2019-04-28 16:52:28 -0400

    confetti

--- src/Container/LostItemForm.js
+++ src/Container/LostItemForm.js
@@ -110,7 +110,9 @@ class LostItemForm extends React.Component {
             <Link to="/">
           <img src={Logo} className="img-thumbnail float-right" style={{width:'100px'}} alt=""/>
           </Link>
-          </h1>    
+          </h1>   
+
+        <p>Fill out form to match found item that has been entered in our website to claim your lost item</p>
         </div>
         <div className="form-group">
           <label>TYPE</label>
