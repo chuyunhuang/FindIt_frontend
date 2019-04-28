d734deb1ceda89ed59d872d8fceb27b87a9242ad
Chuyun Huang <chuyunhuang@pursuit.org>
2019-04-28 18:02:36 -0400

    Updates

---
+++ precache-manifest.782a806042d409fa0a984dde9050c41c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef2f57f21a53bdd5ee3556ec9619c8b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66b8068ccd106b53e642",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "e4a267b77826e7827d16",
+    "url": "/static/css/main.4a4bedde.chunk.css"
+  },
+  {
+    "revision": "66b8068ccd106b53e642",
+    "url": "/static/js/2.cb1db2a1.chunk.js"
+  },
+  {
+    "revision": "e4a267b77826e7827d16",
+    "url": "/static/js/main.7c8846ce.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d737ce970e554dc3c3d1ce69654b3bc0",
+    "url": "/static/media/boy.d737ce97.jpg"
+  },
+  {
+    "revision": "0c25c650edebf7945432628c0318ce54",
+    "url": "/static/media/findIt.0c25c650.jpg"
+  },
+  {
+    "revision": "a6b56fcdd7eca3705c87025ae871f227",
+    "url": "/static/media/girl.a6b56fcd.jpg"
+  },
+  {
+    "revision": "29be05636a5743dc07d78045df0c00cc",
+    "url": "/static/media/nyc.29be0563.jpg"
+  }
+]);
